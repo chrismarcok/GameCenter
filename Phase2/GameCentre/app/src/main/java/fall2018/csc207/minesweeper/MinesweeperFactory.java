6f046c166903dd2599a4f4919be919a88304c76b
tommyliu9 <toms.liu@mail.utoronto.ca>
2018-11-19 21:54:55 -0500

    Implemented the settings MinesweeperFactory.java

--- Phase2/GameCentre/app/src/main/java/fall2018/csc207/minesweeper/MinesweeperFactory.java
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc207/minesweeper/MinesweeperFactory.java
@@ -1,4 +1,53 @@
 package fall2018.csc207.minesweeper;
 
-public class MinesweeperFactory {
+import java.util.Arrays;
+import java.util.List;
+
+import fall2018.csc207.game.GameFactory;
+import fall2018.csc207.game.GameState;
+
+public class MinesweeperFactory extends GameFactory {
+    /*
+     * The settings for the Minesweeper game.
+     */
+    public MinesweeperFactory() {
+        addToSettings(new Setting(
+                "Difficulty",
+                Arrays.asList("Easy", "Medium", "Hard", "MEGA-HARD!!!!"),
+                "Easy"
+        ));
+    }
+
+    @Override
+    public GameState getGameState(int numUndos) {
+        Board board;
+        switch (settings.get(0).getCurrentValue()) { //There should only be 1 thing in settings anyways
+            case "Easy":
+                board = new Board(4, .1);
+                break;
+            case "Medium":
+                board = new Board(8, .1);
+                break;
+            case "Hard":
+                board = new Board(10, .2);
+                break;
+            case "MEGA-HARD!!!!":
+                board = new Board(15, .5);
+                break;
+            default:
+                throw new IllegalStateException("This factory's settings is in an illegal state!");
+        }
+        board.setMaxUndos(numUndos);
+        return board;
+    }
+
+    @Override
+    public Class getGameFragmentClass() {
+        return MinesweeperFragment.class;
+    }
+
+    @Override
+    public List<String> getGameNames() {
+        return Arrays.asList("Minesweeper Easy", "Minesweeper Medium","Minesweeper Hard","Minesweeper Impossible");
+    }
 }
