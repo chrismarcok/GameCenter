cd70ee2e09e0cb49135ff0dfcd7f33dd96b627c8
Steven Kang <stevenkang84@yahoo.ca>
2018-11-21 00:25:59 -0500

    revealSurrondingBlanks correction

--- Phase2/GameCentre/app/src/main/java/fall2018/csc207/minesweeper/Board.java
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc207/minesweeper/Board.java
@@ -142,25 +142,25 @@ public class Board extends GameState {
      */
     public void revealSurrondingBlanks(int row, int col){
          //Reveal the 4 surronding tiles that aren't Bombs, if a blank is revealed reveal the tiles around that blank as well
-        if row+1 < dimensions-1 and mineField[row+1][col].getId() != Tile.BOMB and mineField[row+1][col].getrevealed == false{
+        if (row+1 < dimensions-1 && mineField[row+1][col].getId() != Tile.BOMB && mineField[row+1][col].getrevealed == false){
             revealTile(row+1,col);
             if mineField[row+1][col].getId() == Tile.BLANK_TILE{
                 revealSurrondingBlanks(row+1,col);
             }
         }
-        if row-1 < dimensions-1 and mineField[row+1][col].getId() != Tile.BOMB and mineField[row-1][col].getrevealed == false{
+        if (row-1 < dimensions-1 && mineField[row-1][col].getId() != Tile.BOMB && mineField[row-1][col].getrevealed == false){
             revealTile(row-1,col);
             if mineField[row+1][col].getId() == Tile.BLANK_TILE{
                 revealSurrondingBlanks(row-1,col);
             }
-        }
-        if col+1 < dimensions-1 and mineField[row+1][col].getId() != Tile.BOMB and mineField[row][col + 1].getrevealed == false{
+    }
+        if (col+1 < dimensions-1 && mineField[row][col+1].getId() != Tile.BOMB && mineField[row][col + 1].getrevealed == false){
             revealTile(row,col+1);
             if mineField[row][col+1].getId() == Tile.BLANK_TILE{
                 revealSurrondingBlanks(row,col + 1);
             }
         }
-        if col - 1 < dimensions-1 and mineField[row+1][col].getId() != Tile.BOMB and mineField[row][col - 1].getrevealed == false{
+        if (col - 1 < dimensions-1 && mineField[row][col-1].getId() != Tile.BOMB && mineField[row][col - 1].getrevealed == false){
             revealTile(row,col-1);
             if mineField[row][col-1].getId() == Tile.BLANK_TILE{
                 revealSurrondingBlanks(row,col-1);
