a457f81033fa29e7afc3bb1a2399cd406245d6b7
Michelle Luo <michelleluo99@gmail.com>
2018-11-22 23:04:33 -0500

    Got the grid of 2048 to work and generate blank tiles, now I have to generate random tiles of 2 or 4 when you swipe.

---
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc207/twentyfortyeight/BoardManager.java
@@ -0,0 +1,68 @@
+package fall2018.csc207.twentyfortyeight;
+
+import android.support.v4.util.Pair;
+
+import java.io.Serializable;
+
+import fall2018.csc207.game.GameManager;
+import fall2018.csc207.twentyfortyeight.Board;
+
+/**
+ * Manage the board state by processing taps.
+ */
+public class BoardManager extends GameManager<Board> implements Serializable {
+
+    /**
+     * The dimension of the board
+     */
+    private int dimensions;
+
+    /**
+     * Manage a board that has been pre-populated.
+     *
+     * @param board the board
+     */
+    public BoardManager(Board board) {
+        super(board);
+        dimensions = (int)Math.sqrt(this.gameState.getDimensions());
+    }
+
+    /**
+     * Processes user input and updates tile game accordingly.
+     * @param position The position of the input.
+     */
+    public void updateGame(int position) {
+        touchMove(position);
+    }
+
+    /**
+     * Return whether any of the four surrounding tiles is the blank tile.
+     *
+     * @param position the tile to check
+     * @return whether the tile at position is surrounded by a blank tile
+     */
+    protected boolean isValidTap(int position) {
+//        int row = position / dimensions;
+//        int col = position % dimensions;
+//        int blankId = gameState.numTiles();
+//        return gameState.findEmptyTileAdjacent(row, col, blankId, false) != null;
+        return true;
+    }
+
+    /**
+     * Process a touch at position in the board, swapping tiles as appropriate.
+     *
+     * @param position the position
+     */
+    private void touchMove(int position) {
+
+//        int row = position / dimensions;
+//        int col = position % dimensions;
+//        int blankId = gameState.numTiles();
+//
+//        // tiles is the blank tile, swap by calling Board's swap method.
+//        Pair<Integer, Integer> blankCoord = gameState.findEmptyTileAdjacent(row, col, blankId, false);
+//        if (blankCoord != null)
+//            gameState.swapTiles(blankCoord.first, blankCoord.second, row, col, true);
+    }
+}
