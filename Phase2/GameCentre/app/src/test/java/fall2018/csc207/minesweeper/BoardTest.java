9bf8a02b15e8edff749791702a281acfb4011088
tommyliu9 <toms.liu@mail.utoronto.ca>
2018-11-26 18:13:36 -0500

    Updated board to 86% coverage and tile to 92% coverage

--- Phase2/GameCentre/app/src/test/java/fall2018/csc207/minesweeper/BoardTest.java
+++ Phase2/GameCentre/app/src/test/java/fall2018/csc207/minesweeper/BoardTest.java
@@ -7,16 +7,18 @@ import static org.junit.Assert.*;
 public class BoardTest {
 
 
-    @Test
-
-    public void testGenerateBoard() {
+    public int [][] generate4x4Board(){
         int[][] test_board = new int[][]{
                 {0, 0, 0, 0},
                 {0, 0, 0, 0},
                 {0, 1, 1, 1},
                 {0, 1, -1, 1}
         };
-        Board board = new Board(test_board);
+        return test_board;
+    }
+
+    public void testGenerateBoard() {
+        Board board = new Board(generate4x4Board());
         assert board.getNumMines() == 1;
     }
     @Test
@@ -28,13 +30,7 @@ public class BoardTest {
     }
     @Test
     public void testRevealTiles(){
-        int[][] test_board = new int[][]{
-                {0, 0, 0, 0},
-                {0, 0, 0, 0},
-                {0, 1, 1, 1},
-                {0, 1, -1, 1}
-        };
-        Board board = new Board(test_board);
+        Board board = new Board(generate4x4Board());
         board.revealSurroundingBlanks(0,0);
         int num = 0;
         for (int i = 0; i < board.getDimensions(); i++){
@@ -46,6 +42,32 @@ public class BoardTest {
         }
         assert num == 14;
     }
-
+    @Test
+    public void testisOver(){
+        Board board = new Board(generate4x4Board());
+        board.revealSurroundingBlanks(0,0);
+        board.revealTile(3,2);
+        assert board.isOver() == true;
+    }
+    @Test
+    public void testisOverWithFlag(){
+        Board board = new Board(generate4x4Board());
+        board.revealSurroundingBlanks(0,0);
+        board.revealTile(3,3);
+        board.flagTile(3,2);
+        assert board.isOver() == true;
+    }
+    @Test
+    public void testGetName(){
+        Board board = new Board(generate4x4Board());
+        assert board.getGameName().equals("Minesweeper 4x4");
+    }
+    @Test
+    public void testDecrementScore(){
+        Board board = new Board(generate4x4Board());
+        board.decrementScore();
+        board.decrementScore();
+        assert board.getScore() == (100000-2);
+    }
 
 }
