ce4cc7d73e0962eef3cc9478e26341988aa45e2c
tommyliu9 <toms.liu@mail.utoronto.ca>
2018-11-27 22:00:40 -0500

    Added Minesweeperfactory coverage

---
+++ Phase2/GameCentre/app/src/test/java/fall2018/csc207/minesweeper/MinesweeperFactoryTest.java
@@ -0,0 +1,29 @@
+package fall2018.csc207.minesweeper;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import fall2018.csc207.game.GameFactory;
+import fall2018.csc207.game.GameState;
+
+public class MinesweeperFactoryTest {
+    @Test
+    public void testFactoryConstructor(){
+        MinesweeperFactory newFactory = new MinesweeperFactory();
+        MinesweeperBoard board = (MinesweeperBoard)newFactory.getGameState(0);
+        assert board.getDimensions() == 5;
+    }
+    @Test
+    public void testgetGameFragmentClass(){
+        MinesweeperFactory newFactory = new MinesweeperFactory();
+        assert newFactory.getGameFragmentClass() == MinesweeperFragment.class;
+    }
+    @Test
+    public void testgetGamenames(){
+        MinesweeperFactory newFactory = new MinesweeperFactory();
+        List b = newFactory.getGameNames();
+        assert b.equals(Arrays.asList("Minesweeper 5x5", "Minesweeper 8x8","Minesweeper 13x13","Minesweeper 20x20"));
+    }
+
+}
