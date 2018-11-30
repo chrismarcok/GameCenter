4104313d478fc8537b30a7fa946a2e124ded014a
tommyliu9 <toms.liu@mail.utoronto.ca>
2018-11-29 20:34:54 -0500

    Refactored TwentyFortyEightController.java with javadoc

--- Phase2/GameCentre/app/src/main/java/fall2018/csc207/twentyfortyeight/TwentyFortyEightController.java
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc207/twentyfortyeight/TwentyFortyEightController.java
@@ -5,7 +5,7 @@ import java.io.Serializable;
 import fall2018.csc207.game.BoardController;
 
 /**
- * Manage the board state by processing taps.
+ * Manage the board state by processing swipes.
  */
 public class TwentyFortyEightController extends BoardController<TwentyFortyEightBoard> implements Serializable {
 
@@ -18,28 +18,52 @@ public class TwentyFortyEightController extends BoardController<TwentyFortyEight
         super(twentyFortyEightBoard);
     }
 
+    /**
+     * Moves all the tiles right
+     */
     public void moveRight() {
         gameState.moveRight();
         gameState.afterMoveActions(true);
     }
+
+    /**
+     * Moves all the tiles left
+     */
     public void moveLeft() {
         gameState.moveLeft();
         gameState.afterMoveActions(true);
     }
+
+    /**
+     * Moves all the tiles up
+     */
     public void moveUp() {
         gameState.moveUp();
         gameState.afterMoveActions(true);
     }
+
+    /**
+     * Moves all the tiles down
+     */
     public void moveDown() {
         gameState.moveDown();
         gameState.afterMoveActions(true);
     }
 
+    /**
+     * Gesture not used in 2048
+     * @param position The position on the grid.
+     */
     @Override
     protected void updateGame(int position) {
 
     }
 
+    /**
+     * Gesture not used in 2048
+     * @param position of the tap
+     * @return false
+     */
     @Override
     protected boolean isValidTap(int position) {
         return false;
