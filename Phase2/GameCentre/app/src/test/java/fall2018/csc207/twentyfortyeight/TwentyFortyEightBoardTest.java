5b0d3434b10f2799166de6c3a59387ee70f06d52
Steven Kang <stevenkang84@yahoo.ca>
2018-11-29 19:44:37 -0500

    twentyforeight test

--- Phase2/GameCentre/app/src/test/java/fall2018/csc207/twentyfortyeight/TwentyFortyEightBoardTest.java
+++ Phase2/GameCentre/app/src/test/java/fall2018/csc207/twentyfortyeight/TwentyFortyEightBoardTest.java
@@ -6,7 +6,7 @@ public class TwentyFortyEightBoardTest {
 
     public void contructorTest(){
         TwentyFortyEightBoard twentyFortyEightBoard = new TwentyFortyEightBoard(6);
-
+        assert twentyFortyEightBoard.getNumActiveTiles() == 2;
     }
 
     public void contructorGivenTilesTest(){
@@ -14,10 +14,10 @@ public class TwentyFortyEightBoardTest {
     }
 
     public void movementTest(){
-
+        //TO:DO: Needs to populate specific board to test functionalities.
     }
 
     public void isOverTest(){
-
+        //TO:DO: Needs to populate specific board to test functionalities.
     }
 }
