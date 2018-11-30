904d4d14cec47ed93fdafa33544c780f3e7cc04f
Weiran Gu <weirangu@gmail.com>
2018-11-30 04:13:51 -0500

    Pushed 2048 tests.

---
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc207/twentyfortyeight/GestureDetectGridView2048.java
@@ -0,0 +1,128 @@
+package fall2018.csc207.twentyfortyeight;
+
+/*
+Adapted from:
+https://github.com/DaveNOTDavid/
+sample-puzzle/blob/master/app/src/main/java/com/davenotdavid/samplepuzzle/GestureDetectGridView.java
+
+This extension of GridView contains built in logic for handling swipes between buttons
+ */
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.widget.GridView;
+
+public class GestureDetectGridView2048 extends GridView {
+    private static final int SWIPE_MIN_DISTANCE = 100;
+    private static final int SWIPE_THRESHOLD = 100;
+    private static final int SWIPE_VELOCITY_THRESHOLD = 100;
+    private GestureDetector gDetector;
+    private boolean mFlingConfirmed;
+    private float mTouchX;
+    private float mTouchY;
+    private TwentyFortyEightController controller;
+
+    public GestureDetectGridView2048(Context context) {
+        super(context);
+        init(context);
+    }
+
+    public GestureDetectGridView2048(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+    public GestureDetectGridView2048(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context);
+    }
+
+    private void init(final Context context) {
+        gDetector = new GestureDetector(context, new GestureDetector.SimpleOnGestureListener() {
+
+            @Override
+            public boolean onDown(MotionEvent event) {
+                return true;
+            }
+
+            @Override
+            public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
+                                   float velocityY) {
+                boolean result = false;
+                try {
+                    float diffY = e2.getY() - e1.getY();
+                    float diffX = e2.getX() - e1.getX();
+                    if (Math.abs(diffX) > Math.abs(diffY)) {
+                        if (Math.abs(diffX) > SWIPE_THRESHOLD && Math.abs(velocityX) >
+                                SWIPE_VELOCITY_THRESHOLD) {
+                            if (diffX > 0) {
+                                controller.moveRight();
+                            } else {
+                                controller.moveLeft();
+                            }
+                            result = true;
+                        }
+                    } else if (Math.abs(diffY) > SWIPE_THRESHOLD && Math.abs(velocityY) >
+                            SWIPE_VELOCITY_THRESHOLD) {
+                        if (diffY > 0) {
+                            controller.moveDown();
+                        } else {
+                            controller.moveUp();
+                        }
+                        result = true;
+                    }
+                } catch (Exception exception) {
+                    exception.printStackTrace();
+                }
+                return result;
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        int action = ev.getActionMasked();
+        gDetector.onTouchEvent(ev);
+
+        if (action == MotionEvent.ACTION_CANCEL || action == MotionEvent.ACTION_UP) {
+            mFlingConfirmed = false;
+        } else if (action == MotionEvent.ACTION_DOWN) {
+            mTouchX = ev.getX();
+            mTouchY = ev.getY();
+        } else {
+
+            if (mFlingConfirmed) {
+                return true;
+            }
+
+            float dX = (Math.abs(ev.getX() - mTouchX));
+            float dY = (Math.abs(ev.getY() - mTouchY));
+            if ((dX > SWIPE_MIN_DISTANCE) || (dY > SWIPE_MIN_DISTANCE)) {
+                mFlingConfirmed = true;
+                return true;
+            }
+        }
+
+        return super.onInterceptTouchEvent(ev);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        if (ev.getAction() == MotionEvent.ACTION_UP) {
+            performClick();
+        }
+        return gDetector.onTouchEvent(ev);
+    }
+
+    @Override
+    public boolean performClick() {
+        return super.performClick();
+    }
+
+    public void setController(TwentyFortyEightController controller) {
+        this.controller = controller;
+    }
+}
--- Phase2/GameCentre/app/src/test/java/fall2018/csc207/twentyfortyeight/TwentyFortyEightControllerTest.java
+++ Phase2/GameCentre/app/src/test/java/fall2018/csc207/twentyfortyeight/TwentyFortyEightControllerTest.java
@@ -1,16 +1,20 @@
 package fall2018.csc207.twentyfortyeight;
 
-public class TwentyFortyEightControllerTest {
+import java.util.ArrayList;
 
-//    private void generateBoard() {
-//        ArrayList<TwentyFortyEightTile> tiles = new ArrayList<>(16);
-//
-//        for (int row = 0; row != 4; row++) {
-//            for (int col = 0; col != 4; col++) {
-//                TwentyFortyEightTile newTile = new TwentyFortyEightTile(0);
-//                tiles.add(newTile);
-//            }
-//        }
-//        return tiles;
-//    }
+public class TwentyFortyEightControllerTest {
+    private TwentyFortyEightController board;
+
+
+    private ArrayList<TwentyFortyEightTile> generateBoard() {
+        ArrayList<TwentyFortyEightTile> tiles = new ArrayList<>(16);
+
+        for (int row = 0; row != 4; row++) {
+            for (int col = 0; col != 4; col++) {
+                TwentyFortyEightTile newTile = new TwentyFortyEightTile(0);
+                tiles.add(newTile);
+            }
+        }
+        return tiles;
+    }
 }
